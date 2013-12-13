51c25ac600eafe62da91c5f54d28c01e80bddbb3
Arne Vonheim <vonheim74@gmail.com>
2013-12-13 13:46:59 +0100

    Added example

---
+++ examples/app.js
@@ -0,0 +1,15 @@
+var stat = require('./configured-nodestat.js');
+
+stat.add("count-1", 2);
+stat.add("count-2", 3);
+stat.add("count-4", 3);
+stat.add("total", 13);
+stat.add("category-1", 4);
+stat.add("category-2", 13);
+stat.add("category-3", 8);
+
+
+setInterval(function() {
+  var key = "count-" + Math.floor(Math.random()*7);
+  stat.add(key, 1);
+}, 500);
